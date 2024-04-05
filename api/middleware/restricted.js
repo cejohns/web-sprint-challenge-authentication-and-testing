fa754c469ba2aae6eac7c4b9e016e785c97e8e08
Cory Evan Johnson <cejohns3@gmail.com>
2024-04-05 13:13:56 -0400

    Done 3

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -2,7 +2,7 @@
 const jwt = require('jsonwebtoken');
 
 // Assuming 'YOUR_SECRET_KEY' is the secret key you used to sign the JWTs.
-const SECRET_KEY = process.env.SECRET || 'ssh';
+const SECRET_KEY = process.env.SECRET || 'the secret';
 
 
 
